5785595bc483e73310546c65efc27e42cf7b37e7
Benj Sicam <benj@codechef.io>
2016-09-15 15:21:23 +0800

    Minor enhancements

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 'use strict';
 
-var platform = require('./platform'),
-	gcloud = require('gcloud'),
-	async = require('async'),
+var platform      = require('./platform'),
+	async         = require('async'),
+	gcloud        = require('google-cloud'),
+	isArray       = require('lodash.isarray'),
 	isPlainObject = require('lodash.isplainobject'),
-	isArray = require('lodash.isarray'),
-	path = require('path'),
-	gcloudOptions,
-	connection;
-
+	config, connection;
 
 let saveData = function (entity, done) {
 	connection.save({
-		key: gcloudOptions.key,
+		key: config.key,
 		data: entity
 	}, (err) => {
-		if(! err) {
-			let insertedData = {
+		if (!err) {
+			platform.log(JSON.stringify({
 				title: 'Entity successfuly saved to Google Cloud Datastore',
 				entity: entity
-			};
-
-			platform.log(JSON.stringify(insertedData));
+			}));
 		}
 
 		done(err);
@@ -32,7 +27,6 @@ let saveData = function (entity, done) {
  * @param {object} data The data coming from the device represented as JSON Object.
  */
 platform.on('data', function (data) {
-
 	if (isPlainObject(data)) {
 		saveData(data, (error) => {
 			if (error) platform.handleException(error);
@@ -45,15 +39,15 @@ platform.on('data', function (data) {
 			if (error) platform.handleException(error);
 		});
 	}
-	else {
+	else
 		platform.handleException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`));
-	}
 });
 
 /**
  * Emitted when the platform shuts down the plugin. The Storage should perform cleanup of the resources on this event.
  */
 platform.once('close', function () {
+	connection = null;
 	platform.notifyClose();
 });
 
@@ -67,8 +61,11 @@ platform.once('ready', function (options) {
 
 	d.once('error', (error) => {
 		platform.handleException(error);
-		platform.notifyClose();
 		d.exit();
+
+		setTimeout(() => {
+			process.exit(1);
+		}, 5000);
 	});
 
 	d.run(() => {
@@ -81,7 +78,7 @@ platform.once('ready', function (options) {
 		});
 
 		options.key = connection.key(options.key);
-		gcloudOptions = options;
+		config = options;
 
 		platform.notifyReady();
 		platform.log('Google Cloud Datastore storage has been initialized.');
