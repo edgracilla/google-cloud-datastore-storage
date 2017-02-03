03bc0dd85cd8fe334aebf8f77dff64d817a49429
edgracilla <ed.gracilla@gmail.com>
2017-02-03 14:47:07 +0800

    Migrated - utilizing v3 Plugin SDK

--- app.js
+++ app.js
@@ -1,86 +1,80 @@
-'use strict';
+'use strict'
 
-var platform      = require('./platform'),
-	async         = require('async'),
-	gcloud        = require('google-cloud'),
-	isArray       = require('lodash.isarray'),
-	isPlainObject = require('lodash.isplainobject'),
-	config, connection;
+const reekoh = require('demo-reekoh-node')
+const _plugin = new reekoh.plugins.Storage()
 
-let saveData = function (entity, done) {
-	connection.save({
-		key: config.key,
-		data: entity
-	}, (err) => {
-		if (!err) {
-			platform.log(JSON.stringify({
-				title: 'Entity successfuly saved to Google Cloud Datastore',
-				entity: entity
-			}));
-		}
+const isArray = Array.isArray
+const async = require('async')
+const gcloud = require('google-cloud')
+const isPlainObject = require('lodash.isplainobject')
 
-		done(err);
-	});
-};
-/**
- * Emitted when device data is received. This is the event to listen to in order to get real-time data feed from the connected devices.
- * @param {object} data The data coming from the device represented as JSON Object.
- */
-platform.on('data', function (data) {
-	if (isPlainObject(data)) {
-		saveData(data, (error) => {
-			if (error) platform.handleException(error);
-		});
-	}
-	else if (isArray(data)) {
-		async.each(data, (datum, done) => {
-			saveData(datum, done);
-		}, (error) => {
-			if (error) platform.handleException(error);
-		});
-	}
-	else
-		platform.handleException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`));
-});
+let connection = {}
 
-/**
- * Emitted when the platform shuts down the plugin. The Storage should perform cleanup of the resources on this event.
- */
-platform.once('close', function () {
-	connection = null;
-	platform.notifyClose();
-});
+let _options = {
+  key: process.env.GCDS_KEY,
+  project_id: process.env.GCDS_PROJECT_ID,
+  client_email: process.env.GCDS_CLIENT_EMAIL,
+  private_key: process.env.GCDS_PRIVATE_KEY
+}
 
-/**
- * Emitted when the platform bootstraps the plugin. The plugin should listen once and execute its init process.
- * Afterwards, platform.notifyReady() should be called to notify the platform that the init process is done.
- * @param {object} options The options or configuration injected by the platform to the plugin.
- */
-platform.once('ready', function (options) {
-	let d = require('domain').create();
+let saveData = (entity, done) => {
+  connection.save({
+    key: _options.key,
+    data: entity
+  }, (err) => {
+    if (!err) {
+      _plugin.log(JSON.stringify({
+        title: 'Entity successfuly saved to Google Cloud Datastore',
+        entity: entity
+      }))
+    }
 
-	d.once('error', (error) => {
-		platform.handleException(error);
-		d.exit();
+    done(err)
+  })
+}
 
-		setTimeout(() => {
-			process.exit(1);
-		}, 5000);
-	});
+_plugin.on('data', (data) => {
+  if (isPlainObject(data)) {
+    saveData(data, (err) => {
+      if (err) return _plugin.logException(err)
+      process.send({ type: 'processed' })
+    })
+  } else if (isArray(data)) {
+    async.each(data, (datum, done) => {
+      saveData(datum, done)
+    }, (err) => {
+      if (err) return _plugin.logException(err)
+      process.send({ type: 'processed' })
+    })
+  } else {
+    _plugin.logException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${data}`))
+  }
+})
 
-	d.run(() => {
-		connection = gcloud.datastore({
-			projectId: options.project_id,
-			credentials: {
-				client_email: options.client_email,
-				private_key: options.private_key
-			}
-		});
+_plugin.once('ready', () => {
+  let d = require('domain').create()
 
-		options.key = connection.key(options.key);
-		config = options;
+  d.once('error', (error) => {
+    _plugin.logException(error)
+    d.exit()
 
-		platform.notifyReady();
-		platform.log('Google Cloud Datastore storage has been initialized.');
-	});
-});
+    setTimeout(() => {
+      process.exit(1)
+    }, 5000)
+  })
+
+  d.run(() => {
+
+    connection = gcloud.datastore({
+      projectId: _options.project_id,
+      credentials: {
+        client_email: _options.client_email,
+        private_key: _options.private_key
+      }
+    })
+
+    _options.key = connection.key(_options.key)
+    _plugin.log('Google Cloud Datastore storage has been initialized.')
+    process.send({ type: 'ready' })
+  })
+})
--- test/storage.test.js
+++ test/storage.test.js
@@ -1,97 +1,114 @@
-'use strict';
+/* global describe, it, before, after */
 
+'use strict'
+
+const KEY = 'Plugin'
 const GCDS_CONFIG = {
-		  projectId: 'reekoh-plugins',
-		  credentials: {
-			  client_email: 'reekoh-plugin@reekoh-plugins.iam.gserviceaccount.com',
-			  private_key: `-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCcZ9kYUyResBfz\n2Gg9eW43xH+MLOJtFmk6nGWN79jKg5h/oZA/cybQYXwsdAXCrOJ6NqbeWV31Giia\ndKnYDTGyRjjLlfpV1TjLKq9jgZUzEaTO9Eyvu/vatFP3wP6U6ysD3PIHua7sW+PZ\naMN1kOvau/vC7PnL1tiBaHyGmcWlXr3LRKefjr0d7JvLUfsMvGZK+cDRCS/A7Bcq\nNumJbco+MqTt2APzjraDdWvTClqS+4/ewQlnY3JB9L+HoewCNuO/ihSNoXjBiRgC\n37fgFsIBWcVqwooVOS/s3Qrx7b/pmyLO6OzPZ0ln6T5m4cFNMQ9asZJOCFeVqs3U\nXQtLeOnZAgMBAAECggEAVW7MQOM5XJdnngS5e8rWj5O/7/uceuXg+Ojw+ATpMEWN\niTnhJydpLi3xu/5sHxh8ZTFxGMdL8VQmlafgFmmumaiN4Xa81SGw8hkQ0JO5wbmD\ngqAjO2K9gYs6ynwbE5EZX/TSJ7IfgG/5F4qIKbHNgx9nt+Pl1pg2jwlZ69QXB+rH\nSVDaH5D1xqQYjEqR0KZQcqpSkk+UqIII16OxVsEcrUE6kS94/PgtZyLDO6PB44Nt\nthLLC8F3MNqWFkV0hGzykv9VISwqaL37dDKrP4z7i+yGhMJ+XNZl3w9wiW3jAZq0\n6P2ho7L30J54gsjjTyGBm4s1Gt2M6EhIbbgtMTAkAQKBgQD0uH1/8qUbXApUn+w6\nliigSuwJMR9+Ix8fttqsTnyVAVU6HieU6ZMFQcQZVRt4XzgONQ3G6d6OSaP4qr/f\naNPYUED9vl1N9dyfswI0LVv8tpNHMXEqgR40TXxbM/tW3LWVNiPDLuAG7A/r7Ooa\nB2Ty+zmQV60me1BOThKxt5NeQQKBgQCjnU+MIg5zfeBlWFBEy5cZGtnVVzZZIFlD\npGn6E7Fon7lrTxTABDYAPWLtf9BhOkpevWQhY/T45Gh4IshQPIZWBcMz5bKV1C5O\ncoTxdCs8zA4POj2DdDSX89a2oSrUm88fhu5dEETtpPsVDXkeZdfkCtImymoRaOw8\nOz2zM5pVmQKBgQCHO8xegEAtNlefHCD23zPkIIL/MV4t3kq8w9djkJvX27y1OSS2\nbb0JEqbDOPzNMBFSCI9sdKDAEIHEUNdnJg6rYu4HSsNU6aDZv5pgKxDCPkJ0/SWE\nT4XZcVhfLgKepajSo/BvU+AI8zp5LZrQd3WZsKrG0SymL07ZdtttyfxuAQKBgHq5\n+jxMI3jgF90NJJCmAj3zlShhFcQ+7pY9krzh3rZy8PucQx1RS0xcv4MU17LGrZhp\nCd2+A7haZD6RIJ6G9+E4y1Z3WsDmeqGfZzdSg0hEa9tiPVgIZ5b/cbRmKiF2iiPE\n67FlDb8QfMIq7AowLIPC1iYAuRU8Fc+MRFjSQVcpAoGAdASUtw/DCP4Td7u86LRG\nw4lBudibMZsaM+fnDfO9ZpcfGGIC0nSfOu0vTpULXVtcfWU0zV1lHjVr5imWwrAF\nkUI1hY6CW1ID/yBjDcxgl4aksLuCmGBd/u7Aners9kikmLk2zO6ZsF823VPesSpC\ngbKF2ESAi9rrU+bBN44U2tU=\n-----END PRIVATE KEY-----\n`,
-		  }
-	  },
-	  KEY         = 'Plugin';
+  projectId: 'reekoh-plugins',
+  credentials: {
+    client_email: 'reekoh-plugin@reekoh-plugins.iam.gserviceaccount.com',
+    private_key: `-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCcZ9kYUyResBfz\n2Gg9eW43xH+MLOJtFmk6nGWN79jKg5h/oZA/cybQYXwsdAXCrOJ6NqbeWV31Giia\ndKnYDTGyRjjLlfpV1TjLKq9jgZUzEaTO9Eyvu/vatFP3wP6U6ysD3PIHua7sW+PZ\naMN1kOvau/vC7PnL1tiBaHyGmcWlXr3LRKefjr0d7JvLUfsMvGZK+cDRCS/A7Bcq\nNumJbco+MqTt2APzjraDdWvTClqS+4/ewQlnY3JB9L+HoewCNuO/ihSNoXjBiRgC\n37fgFsIBWcVqwooVOS/s3Qrx7b/pmyLO6OzPZ0ln6T5m4cFNMQ9asZJOCFeVqs3U\nXQtLeOnZAgMBAAECggEAVW7MQOM5XJdnngS5e8rWj5O/7/uceuXg+Ojw+ATpMEWN\niTnhJydpLi3xu/5sHxh8ZTFxGMdL8VQmlafgFmmumaiN4Xa81SGw8hkQ0JO5wbmD\ngqAjO2K9gYs6ynwbE5EZX/TSJ7IfgG/5F4qIKbHNgx9nt+Pl1pg2jwlZ69QXB+rH\nSVDaH5D1xqQYjEqR0KZQcqpSkk+UqIII16OxVsEcrUE6kS94/PgtZyLDO6PB44Nt\nthLLC8F3MNqWFkV0hGzykv9VISwqaL37dDKrP4z7i+yGhMJ+XNZl3w9wiW3jAZq0\n6P2ho7L30J54gsjjTyGBm4s1Gt2M6EhIbbgtMTAkAQKBgQD0uH1/8qUbXApUn+w6\nliigSuwJMR9+Ix8fttqsTnyVAVU6HieU6ZMFQcQZVRt4XzgONQ3G6d6OSaP4qr/f\naNPYUED9vl1N9dyfswI0LVv8tpNHMXEqgR40TXxbM/tW3LWVNiPDLuAG7A/r7Ooa\nB2Ty+zmQV60me1BOThKxt5NeQQKBgQCjnU+MIg5zfeBlWFBEy5cZGtnVVzZZIFlD\npGn6E7Fon7lrTxTABDYAPWLtf9BhOkpevWQhY/T45Gh4IshQPIZWBcMz5bKV1C5O\ncoTxdCs8zA4POj2DdDSX89a2oSrUm88fhu5dEETtpPsVDXkeZdfkCtImymoRaOw8\nOz2zM5pVmQKBgQCHO8xegEAtNlefHCD23zPkIIL/MV4t3kq8w9djkJvX27y1OSS2\nbb0JEqbDOPzNMBFSCI9sdKDAEIHEUNdnJg6rYu4HSsNU6aDZv5pgKxDCPkJ0/SWE\nT4XZcVhfLgKepajSo/BvU+AI8zp5LZrQd3WZsKrG0SymL07ZdtttyfxuAQKBgHq5\n+jxMI3jgF90NJJCmAj3zlShhFcQ+7pY9krzh3rZy8PucQx1RS0xcv4MU17LGrZhp\nCd2+A7haZD6RIJ6G9+E4y1Z3WsDmeqGfZzdSg0hEa9tiPVgIZ5b/cbRmKiF2iiPE\n67FlDb8QfMIq7AowLIPC1iYAuRU8Fc+MRFjSQVcpAoGAdASUtw/DCP4Td7u86LRG\nw4lBudibMZsaM+fnDfO9ZpcfGGIC0nSfOu0vTpULXVtcfWU0zV1lHjVr5imWwrAF\nkUI1hY6CW1ID/yBjDcxgl4aksLuCmGBd/u7Aners9kikmLk2zO6ZsF823VPesSpC\ngbKF2ESAi9rrU+bBN44U2tU=\n-----END PRIVATE KEY-----\n`
+  }
+}
 
-var cp     = require('child_process'),
-	should = require('should'),
-	gcloud = require('google-cloud'),
-	d      = new Date(),
-	gcds   = gcloud.datastore(GCDS_CONFIG),
-	storage;
+const amqp = require('amqplib')
+const should = require('should')
+const cp = require('child_process')
+const gcloud = require('google-cloud')
 
-var entity = {
-	'_id': d.valueOf(),
-	'plugin_name': 'Google Cloud Datastore',
-	'created_by': 'Reekoh'
-};
+let d = new Date()
+let gcds = gcloud.datastore(GCDS_CONFIG)
+
+let _channel = null
+let _storage = null
+let _conn = null
+
+let _entity = {
+  '_id': d.valueOf(),
+  'created_by': 'Reekoh',
+  'plugin_name': 'Google Cloud Datastore',
+
+}
 
 describe('Storage', function () {
-	this.slow(5000);
+  this.slow(5000)
 
-	after('terminate child process', function () {
-		storage.send({
-			type: 'close'
-		});
+  before('init', () => {
+    process.env.INPUT_PIPE = 'demo.pipe.storage'
+    process.env.BROKER = 'amqp://guest:guest@127.0.0.1/'
 
-		setTimeout(function () {
-			storage.kill('SIGKILL');
-		}, 3000);
-	});
+    process.env.GCDS_KEY = KEY
+    process.env.GCDS_PROJECT_ID = GCDS_CONFIG.projectId
+    process.env.GCDS_CLIENT_EMAIL = GCDS_CONFIG.credentials.client_email
+    process.env.GCDS_PRIVATE_KEY = GCDS_CONFIG.credentials.private_key
 
-	describe('#spawn', function () {
-		it('should spawn a child process', function () {
-			should.ok(storage = cp.fork(process.cwd()), 'Child process not spawned.');
-		});
-	});
+    amqp.connect(process.env.BROKER)
+      .then((conn) => {
+        _conn = conn
+        return conn.createChannel()
+      }).then((channel) => {
+        _channel = channel
+      }).catch((err) => {
+        console.log(err)
+      })
 
-	describe('#handShake', function () {
-		it('should notify the parent process when ready within 8 seconds', function (done) {
-			this.timeout(8000);
+  })
 
-			storage.on('message', function (message) {
-				if (message.type === 'ready')
-					done();
-			});
+  after('terminate child process', function () {
+    _conn.close()
+    setTimeout(() => {
+      _storage.kill('SIGKILL')
+    }, 3000)
+  })
 
-			storage.send({
-				type: 'ready',
-				data: {
-					options: {
-						project_id: GCDS_CONFIG.projectId,
-						client_email: GCDS_CONFIG.credentials.client_email,
-						private_key: GCDS_CONFIG.credentials.private_key,
-						key: KEY
-					}
-				}
-			}, function (error) {
-				should.ifError(error);
-			});
-		});
-	});
+  describe('#spawn', function () {
+    it('should spawn a child process', function () {
+      should.ok(_storage = cp.fork(process.cwd()), 'Child process not spawned.')
+    })
+  })
 
-	describe('#data', function () {
-		it('should process the data', function (done) {
-			storage.send({
-				type: 'data',
-				data: entity
-			}, done);
-		});
+  describe('#handShake', function () {
+    it('should notify the parent process when ready within 8 seconds', function (done) {
+      this.timeout(8000)
 
-		it('should should verify that the file was inserted', function (done) {
-			this.timeout(10000);
+      _storage.on('message', function (message) {
+        if (message.type === 'ready') {
+          done()
+        }
+      })
+    })
+  })
 
-			let q = gcds.createQuery(KEY).filter('_id', entity._id);
+  describe('#data', function () {
+    
+    it('should process the data', function (done) {
+      this.timeout(8000)
+      _channel.sendToQueue(process.env.INPUT_PIPE, new Buffer(JSON.stringify(_entity)))
 
-			gcds.runQuery(q, function (err, entities, nextQuery) {
+      _storage.on('message', (msg) => {
+        if (msg.type === 'processed') { done() }
+      })
+    })
 
-				should.ifError(err);
-				should.equal(1, entities.length);
+    it('should should verify that the file was inserted', function (done) {
+      this.timeout(10000)
 
-				let retrievedEntity = entities[0];
+      let q = gcds.createQuery(process.env.GCDS_KEY)
+        .filter('_id', _entity._id)
 
-				Object.keys(entity).forEach((propName, index) => {
-					should.equal(entity[propName], retrievedEntity.data[propName], `Entity didn't matched on '${propName}' property`);
-				});
+      gcds.runQuery(q, function (err, entities, nextQuery) {
 
-				done();
-			});
-		});
-	});
-});
+        should.ifError(err)
+        should.equal(1, entities.length)
+
+        let retrievedEntity = entities[0]
+
+        Object.keys(_entity).forEach(propName => {
+          should.equal(_entity[propName], retrievedEntity[propName], `_entity didn't matched on '${propName}' property`)
+        })
+
+        done()
+      })
+    })
+  })
+})
